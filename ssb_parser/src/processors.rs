172656c9f5dd0e20f32d31ad9737c7096b3192e7
Youka <spanknebel.borken@t-online.de>
2019-04-27 01:54:55 +0200

    fixed pest rules matching by atomic inheritance

--- ssb_parser/src/processors.rs
+++ ssb_parser/src/processors.rs
@@ -48,36 +48,49 @@ impl SsbParser {
         let pairs = Parser::parse(Rule::script, script)?;
         // Iterate through section entries
         for section_entry_pair in pairs {
-            println!("{:?}", section_entry_pair.as_rule());
             match section_entry_pair.as_rule() {
                 // Info entries
-                Rule::info_title_value => self.data.info_title = Some(section_entry_pair.as_span().as_str().to_string()),
-                Rule::info_author_value => self.data.info_author = Some(section_entry_pair.as_span().as_str().to_string()),
-                Rule::info_desc_value => self.data.info_description = Some(section_entry_pair.as_span().as_str().to_string()),
-                Rule::info_version_value => self.data.info_version = Some(section_entry_pair.as_span().as_str().to_string()),
-                Rule::info_custom_entry => {
-                    let mut info_custom_entry_pair = section_entry_pair.into_inner();
-                    if let (Some(info_custom_key), Some(info_custom_value)) = (info_custom_entry_pair.next(), info_custom_entry_pair.next()) {
-                        self.data.info_custom.insert(info_custom_key.as_str().to_string(), info_custom_value.as_str().to_string());
+                Rule::info_entry => {
+                    for info_entry_pair in section_entry_pair.into_inner() {
+                        match info_entry_pair.as_rule() {
+                            Rule::info_title_value => self.data.info_title = Some(info_entry_pair.as_span().as_str().to_string()),
+                            Rule::info_author_value => self.data.info_author = Some(info_entry_pair.as_span().as_str().to_string()),
+                            Rule::info_desc_value => self.data.info_description = Some(info_entry_pair.as_span().as_str().to_string()),
+                            Rule::info_version_value => self.data.info_version = Some(info_entry_pair.as_span().as_str().to_string()),
+                            Rule::info_custom_entry => {
+                                let mut info_custom_entry_pair = info_entry_pair.into_inner();
+                                if let (Some(info_custom_key), Some(info_custom_value)) = (info_custom_entry_pair.next(), info_custom_entry_pair.next()) {
+                                    self.data.info_custom.insert(info_custom_key.as_str().to_string(), info_custom_value.as_str().to_string());
+                                }
+                            }
+                            _ => ()
+                        }
                     }
                 }
                 // Target entries
-                Rule::target_width_value => if let Ok(width) = section_entry_pair.as_span().as_str().parse::<u16>() {
-                    self.data.target_width = Some(width);
-                }
-                Rule::target_height_value => if let Ok(height) = section_entry_pair.as_span().as_str().parse::<u16>() {
-                    self.data.target_height = Some(height);
-                }
-                Rule::target_depth_value => if let Ok(depth) = section_entry_pair.as_span().as_str().parse::<u16>() {
-                    self.data.target_width = Some(depth);
-                }
-                Rule::target_view_value => if let Ok(view) = View::from_str(section_entry_pair.as_span().as_str()) {
-                    self.data.target_view = view;
+                Rule::target_entry => {
+                    for target_entry_pair in section_entry_pair.into_inner() {
+                        match target_entry_pair.as_rule() {
+                            Rule::target_width_value => if let Ok(width) = target_entry_pair.as_span().as_str().parse::<u16>() {
+                                self.data.target_width = Some(width);
+                            }
+                            Rule::target_height_value => if let Ok(height) = target_entry_pair.as_span().as_str().parse::<u16>() {
+                                self.data.target_height = Some(height);
+                            }
+                            Rule::target_depth_value => if let Ok(depth) = target_entry_pair.as_span().as_str().parse::<u16>() {
+                                self.data.target_depth = depth;
+                            }
+                            Rule::target_view_value => if let Ok(view) = View::from_str(target_entry_pair.as_span().as_str()) {
+                                self.data.target_view = view;
+                            }
+                            _ => ()
+                        }
+                    }
                 }
                 // Macros entries
                 Rule::macros_entry => {
                     let mut macros_entry_pair = section_entry_pair.into_inner();
-                    if let (Some(macros_key), _, Some(macros_value)) = (macros_entry_pair.next(), macros_entry_pair.next(), macros_entry_pair.next()) {
+                    if let (Some(macros_key), Some(macros_value)) = (macros_entry_pair.next(), macros_entry_pair.next()) {
                         self.data.macros.insert(macros_key.as_str().to_string(), macros_value.as_str().to_string());
                     }
                 }
@@ -85,9 +98,8 @@ impl SsbParser {
                 // TODO: implement rest below
                 Rule::events_entry => (),
                 // Resources entries
-                Rule::resources_font_entry => (),
-                Rule::resources_texture_entry => (),
-                // Unrelevant catches
+                Rule::resources_entry => (),
+                // Unrelevant matches
                 _ => ()
             }
         }
