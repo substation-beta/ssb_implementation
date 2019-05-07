b6d0c788c30b78dfc9b6217849b0ac6a4f75b3c0
Youka <spanknebel.borken@t-online.de>
2019-05-07 09:12:03 +0200

    added general documentation to parser

--- ssb_parser/src/lib.rs
+++ ssb_parser/src/lib.rs
@@ -1,6 +1,17 @@
-// Export error types
+//! Parser component of subtitle format implementation.
+//! ```
+//! let ssb_data = SsbParser::new("...").unwrap().data();
+//! ```
+#![doc(
+    html_logo_url = "https://substation-beta.github.io/logo.png",
+    html_favicon_url  = "https://substation-beta.github.io/logo.png",
+    html_root_url = "https://substation-beta.github.io"
+)]
+
+
+/// Error types covering all bad situations in this crate.
 pub mod error;
-// Export data types
+/// Data containers for all sort of information of this crate.
 pub mod data;
-// Export processors for data
+/// Processors to generate & handle data of this crate.
 pub mod processing;
