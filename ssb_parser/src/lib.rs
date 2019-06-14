68af31032508a38c66502490533f5e0e1c9b975a
Youka <spanknebel.borken@t-online.de>
2019-06-14 09:20:59 +0200

    fixed logo url in docs

--- ssb_parser/src/lib.rs
+++ ssb_parser/src/lib.rs
@@ -20,8 +20,8 @@
 //! println!("{:#?}", ssb_render);
 //! ```
 #![doc(
-    html_logo_url = "https://substation-beta.github.io/logo.png",
-    html_favicon_url  = "https://substation-beta.github.io/logo.png",
+    html_logo_url = "https://substation-beta.github.io/assets/img/logo.png",
+    html_favicon_url  = "https://substation-beta.github.io/assets/img/logo.png",
     html_root_url = "https://substation-beta.github.io"
 )]
 
