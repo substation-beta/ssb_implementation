7959213b92a1ff73605488cbbc5e843971c36bed
Youka <spanknebel.borken@t-online.de>
2019-06-21 19:36:41 +0200

    prepare rendering fundament

--- ssb_renderer/benches/rendering_benches.rs
+++ ssb_renderer/benches/rendering_benches.rs
@@ -3,22 +3,24 @@ use std::{
     convert::TryFrom,
     time::Duration
 };
-use image::RgbaImage;
 use ssb_parser::data::{Ssb,SsbRender};
 use ssb_renderer::rendering::SsbRenderer;
 use microbench::{bench, Options};
+use image::RgbaImage;
 
 
 // Benchmark
 fn main() {
     // Test data
-    let mut img = RgbaImage::new(1920, 1080);
     let renderer = SsbRenderer::new(SsbRender::try_from(Ssb::default()).expect("Default ssb cannot be wrong for rendering!"));
     // Run test
     bench(&Options::default().time(Duration::from_secs(2)), "Basic rendering.", || {
 
+
         // TODO: more complex rendering
-        renderer.render(&mut img)
+        let img = RgbaImage::new(1920, 1080);
+        let _new_img = renderer.render(img).expect("Image rendering mustn't fail!");
+
 
     });
 }
--- ssb_renderer/src/rendering.rs
+++ ssb_renderer/src/rendering.rs
@@ -4,21 +4,31 @@ use image::RgbaImage;
 use super::error::RenderingError;
 
 
-/// Consumes ssb data and renders them on images
+/// Renderer for ssb data on images
 pub struct SsbRenderer {
     data: SsbRender
 }
 impl SsbRenderer {
+    /// Consumes ssb data as rendering blueprint
     pub fn new(data: SsbRender) -> Self {
         Self {
             data
         }
     }
-    pub fn render(&self, img: &mut RgbaImage) -> Result<&Self,RenderingError> {
+    /// Renders on image by ssb and returns new image
+    pub fn render(&self, img: RgbaImage) -> Result<RgbaImage,RenderingError> {
+        let (width, height) = img.dimensions();
+        let mut buffer = img.into_raw();
+
 
         // TODO: whole rendering process
-        if self.data.events.is_empty() { image::imageops::colorops::invert(img); }
+        if self.data.target_depth > 0 {
+            for channel in &mut buffer {
+                *channel = 255u8 - *channel;
+            }
+        }
 
-        Ok(self)
+
+        Ok(RgbaImage::from_raw(width, height, buffer).expect("Couldn't repack image buffer?!"))
     }
 }
