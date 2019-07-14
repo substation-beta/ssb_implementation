719151f327762785a6207d2d287e8f88e20f4fcf
Youka <spanknebel.borken@t-online.de>
2019-07-14 12:32:52 +0200

    added renderer creation & destroy to c api

--- ssb_filter/src/c.rs
+++ ssb_filter/src/c.rs
@@ -1,9 +1,53 @@
 // Imports
 use libc::*;
+use ssb_renderer::{
+    ssb_parser::data::{Ssb, SsbRender},
+    rendering::SsbRenderer
+};
+use std::{
+    convert::TryFrom,
+    error::Error,
+    ffi::CStr,
+    io::Cursor,
+    ptr::{copy, null_mut}
+};
 
 
 /// Get library version as C string.
 #[no_mangle]
 pub extern fn ssb_version() -> *const c_char {
     concat!(env!("CARGO_PKG_VERSION"), "\0").as_ptr() as *const c_char
+}
+
+/// Create renderer instance.
+/// **script** mustn't be *null*.
+/// **error_message** can be *null*.
+#[no_mangle]
+pub extern fn ssb_new_renderer(script: *const c_char, error_message: *mut c_char, error_message_capacity: c_ushort) -> *mut c_void {
+    match ssb_new_renderer_inner(script) {
+        Ok(renderer) => Box::into_raw(Box::new(renderer)) as *mut c_void,
+        Err(err) => {
+            if error_message != null_mut() && error_message_capacity > 0 {
+                let mut msg = err.to_string();
+                msg.truncate((error_message_capacity-1) as usize);
+                msg.push('\0');
+                unsafe {copy(msg.as_ptr() as *const c_char, error_message, msg.len());}
+            }
+            null_mut()
+        }
+    }
+}
+fn ssb_new_renderer_inner(script_file: *const c_char) -> Result<SsbRenderer, Box::<Error>> {
+    Ok(SsbRenderer::new(
+        Ssb::default().parse_owned( Cursor::new(unsafe{ CStr::from_ptr(script_file) }.to_str()?) )
+        .and_then(|ssb| SsbRender::try_from(ssb) )?
+    ))
+}
+
+/// Destroy renderer instance.
+#[no_mangle]
+pub extern fn ssb_destroy_renderer(renderer: *mut c_void) {
+    if renderer != null_mut() {
+        unsafe {Box::from_raw(renderer);}
+    }
 }
--- ssb_filter/tests/c_tests.rs
+++ ssb_filter/tests/c_tests.rs
@@ -2,18 +2,35 @@ mod c_tests {
     // Imports
     use libloading::Library;
     use libc::*;
-    use std::ffi::CStr;
+    use std::{
+        ffi::{CStr, CString},
+        ptr::null_mut
+    };
     include!("platform.irs");    // Tests are separated, thus include code (::dll_path)
 
     #[test]
     fn test_version() {
         let lib = Library::new(platform::dll_path()).expect("Couldn't load DLL!");
         unsafe {
-            let version_fn = lib.get::<unsafe extern fn() -> *const c_char>(b"ssb_version\0").expect("Couldn't load symbol 'version' from DLL!");
+            let version_fn = lib.get::<unsafe extern fn() -> *const c_char>(b"ssb_version\0").expect("Couldn't load symbol 'ssb_version' from DLL!");
             assert_eq!(
                 CStr::from_ptr(version_fn()).to_string_lossy(),
                 env!("CARGO_PKG_VERSION")
             );
+        }
+    }
+
+    #[test]
+    fn test_renderer() {
+        let lib = Library::new(platform::dll_path()).expect("Couldn't load DLL!");
+        unsafe {
+            let new_renderer_fn = lib.get::<unsafe extern fn(*const c_char, *mut c_char, c_ushort) -> *mut c_void>(b"ssb_new_renderer\0").expect("Couldn't load symbol 'ssb_new_renderer' from DLL!");
+            let destroy_renderer_fn = lib.get::<unsafe extern fn(*mut c_void)>(b"ssb_destroy_renderer\0").expect("Couldn't load symbol 'ssb_destroy_renderer' from DLL!");
+            if let Ok(script) = CString::new("#Events\n0-1.|||") {
+                let renderer = new_renderer_fn(script.as_ptr(), null_mut(), 0);
+                assert_ne!(renderer, null_mut());
+                destroy_renderer_fn(renderer);
+            }
         }
     }
 }
