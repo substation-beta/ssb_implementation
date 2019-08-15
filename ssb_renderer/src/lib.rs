cd6a2dc1a6e66e94aa1efd1eb3ca201c7fca527c
Youka <spanknebel.borken@t-online.de>
2019-08-16 00:44:53 +0200

    pass gpgpu features from filter to renderer

--- ssb_renderer/src/lib.rs
+++ ssb_renderer/src/lib.rs
@@ -8,13 +8,13 @@
     html_root_url = "https://substation-beta.github.io"
 )]
 
-/// Rendering backend for general 2D graphics.
+// Rendering backend for general 2D graphics.
 mod g2d;
 /// Supportive types for rendering.
 pub mod types;
 /// High-level rendering interface.
 pub mod rendering;
 
-/// Re-exports (with interfaces required by public users).
+// Re-exports (with interfaces required by public users).
 pub use ssb_parser;
 pub use g2d::image;
