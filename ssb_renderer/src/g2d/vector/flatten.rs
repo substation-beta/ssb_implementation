6af6305367c0a83b9ebdf0e15d74fab6811182e4
Youka <spanknebel.borken@t-online.de>
2020-01-01 15:07:39 +0100

    simplified flatten_arc

--- ssb_renderer/src/g2d/vector/flatten.rs
+++ ssb_renderer/src/g2d/vector/flatten.rs
@@ -12,35 +12,22 @@ pub fn flatten_arc(start_point: Point, center_point: Point, angle: Degree) -> Ve
     if start_point == center_point || angle == 0.0 {
         return vec![start_point];
     }
-    // Vector between points & angle as radians
-    let (vector, angle_rad) = (start_point - center_point, angle.to_radians());
     // Number of required lines
-    let lines_n = angle_rad.abs() * vector.len() as Degree / ARC_LINE_LENGTH;
-    let lines_n_ceil = lines_n.ceil();
-    // Points buffer
-    let mut points = Vec::with_capacity(1 + lines_n_ceil as usize);
-    // Add start point
+    let (mut vector, mut angle_rad) = (start_point - center_point, angle.to_radians());
+    let lines_n = (angle_rad.abs() * vector.len() as Degree / ARC_LINE_LENGTH).ceil() as usize;
+    // Points buffer with start point
+    let mut points = Vec::with_capacity(1 + lines_n);
     points.push(start_point);
-    // Add intermediate points
-    if lines_n >= 1.0 {
-        let (mut vector_part, angle_rad_part) = (vector, angle_rad / lines_n_ceil);
-        let (angle_sin_part, angle_cos_part) = (angle_rad_part.sin(), angle_rad_part.cos());
-        for _ in 1..lines_n_ceil as usize {
-            points.push(center_point + {
-                vector_part = Point {
-                    x: (vector_part.x as Degree * angle_cos_part - vector_part.y as Degree * angle_sin_part) as Coordinate,
-                    y: (vector_part.x as Degree * angle_sin_part + vector_part.y as Degree * angle_cos_part) as Coordinate
-                };
-                vector_part
-            });
-        }
-    }
-    // Add end point
-    if lines_n_ceil > lines_n {
-        let (angle_sin, angle_cos) = (angle_rad.sin(), angle_rad.cos());
-        points.push(center_point + Point {
-            x: (vector.x as Degree * angle_cos - vector.y as Degree * angle_sin) as Coordinate,
-            y: (vector.x as Degree * angle_sin + vector.y as Degree * angle_cos) as Coordinate
+    // Add remaining points
+    angle_rad /= lines_n as Degree;
+    let (angle_sin, angle_cos) = (angle_rad.sin(), angle_rad.cos());
+    for _ in 0..lines_n {
+        points.push(center_point + {
+            vector = Point {
+                x: (vector.x as Degree * angle_cos - vector.y as Degree * angle_sin) as Coordinate,
+                y: (vector.x as Degree * angle_sin + vector.y as Degree * angle_cos) as Coordinate
+            };
+            vector
         });
     }
     // Return points
@@ -116,7 +103,11 @@ mod tests {
         );
         // Complex
         let points = flatten_arc(Point {x: 0.0, y: -4.0}, Point {x: 0.0, y: -2.0}, -270.0);
-        assert_eq!(points.last(), Some(&Point{x: 2.0, y: -2.0}), "Points: {:?}", points);
+        let last_point = points.last().expect("End point is missing!");
+        assert!(
+            (1.99999..2.00001).contains(&last_point.x) && (-2.00001..-1.99999).contains(&last_point.y),
+            "Last point not fitting: {:?}", points
+        );
     }
 
     #[test]
@@ -128,6 +119,6 @@ mod tests {
         );
         // Complex
         let points = flatten_curve(Point {x: -5.0, y: 0.0}, Point {x: 0.0, y: -4.0}, Point {x: 5.0, y: 6.0}, Point {x: 10.0, y: 1.0});
-        assert!(points.len() > 5, "Points: {:?}", points);
+        assert!(points.len() > 5, "Not enough points: {:?}", points);
     }
 }
