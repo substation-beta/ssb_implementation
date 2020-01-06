eae13165764e91ae957685f4c744d7fdcf7e429f
Youka <spanknebel.borken@t-online.de>
2020-01-06 23:48:06 +0100

    begin rasterizer

--- ssb_renderer/src/g2d/raster/mod.rs
+++ ssb_renderer/src/g2d/raster/mod.rs
@@ -1,4 +1,9 @@
 /// Image components for rastering.
 pub mod image;
+/// Alpha image with offset position.
+pub mod mask;
+/// Rasterize to convert path to mask.
+pub mod rasterize;
+
 // Scanline detection for flat paths.
-pub mod scanlines;
+mod scanlines;
---
+++ ssb_renderer/src/g2d/raster/rasterize.rs
@@ -0,0 +1,48 @@
+// Imports
+use crate::g2d::vector::{
+    point::{Point,PointMinMaxCollector},
+    path::FlatPath
+};
+use super::{
+    mask::Mask,
+    scanlines::scanlines_from_path
+};
+use rayon::prelude::*;
+
+
+// Sampling configuration
+const SAMPLE_DEVIATIONS_NUMBER: usize = 4;
+const SAMPLE_DEVIATIONS: [Point;SAMPLE_DEVIATIONS_NUMBER] = [
+    Point {x: -0.25, y: -0.25},
+    Point {x: 0.25, y: -0.25},
+    Point {x: 0.25, y: 0.25},
+    Point {x: -0.25, y: 0.25}
+];
+const SAMPLE_WEIGHT: u8 = {let weight = 256 / SAMPLE_DEVIATIONS_NUMBER; weight - ((weight & 256) >> 8)} as u8;
+
+// Rasterize path to mask
+pub fn rasterize_path(path: &FlatPath, area_width: u16, area_height: u16) -> Option<Mask> {
+    // Create mask
+    let (path_bounding, deviations_bounding) = (path.bounding()?, SAMPLE_DEVIATIONS.iter().min_max()?);
+    let (path_offset, path_peak) = (path_bounding.0 + deviations_bounding.0, path_bounding.1 + deviations_bounding.1);
+    let path_dimensions = path_peak - path_offset;
+
+    // TODO
+
+    // Calculate scanlines in parallel
+    let scanlines = SAMPLE_DEVIATIONS.into_par_iter()
+    .map(|deviation| {
+        let mut new_path = path.clone();
+        new_path.translate(deviation.x, deviation.y);
+        scanlines_from_path(&new_path, area_width, area_height)
+    })
+    .flatten()
+    .collect::<Vec<_>>();
+    // Rasterize scanlines on mask (addition with top-trim)
+    for scanline in scanlines {
+
+        // TODO
+
+    }
+    None
+}
