1a9ad1235fd403fc60edcde0a388c94491d5443b
Youka <spanknebel.borken@t-online.de>
2020-01-11 22:18:59 +0100

    added arc_to_curves function

--- ssb_renderer/src/g2d/vector/flatten.rs
+++ ssb_renderer/src/g2d/vector/flatten.rs
@@ -5,6 +5,82 @@ use super::{
 };
 
 
+// Split arc into cubic curves
+pub fn arc_to_curves(start_point: Point, center_point: Point, angle: Degree) -> Vec<[Point;4]> {
+    // Anything to do?
+    if angle != 0.0 {
+        // Curves buffer
+        let (full_curves_n, last_curve_angle) = (
+            (angle / 90.0).abs().floor() as u16,
+            angle % 90.0
+        );
+        let mut curves = Vec::with_capacity(if last_curve_angle != 0.0 {full_curves_n + 1} else {full_curves_n} as usize);
+        // Magic numbers
+        const CONTROL_POINT_DISTANCE: Degree = 0.388_908_729_652_601;
+        const SIN_COS_45: Coordinate = 0.707_106_781_186_548;
+        // Generate full curves
+        let mut vector = start_point - center_point;
+        let angle_direction = angle.signum() as Coordinate;
+        for _ in 0..full_curves_n {
+            // Calculate end point
+            let rotated_vector = Point {
+                x: -vector.y * angle_direction,
+                y: vector.x * angle_direction
+            };
+            // Calculate vectors to control points
+            let start_end_line = (rotated_vector - vector) * CONTROL_POINT_DISTANCE as Coordinate * SIN_COS_45;
+            let end_start_line = -start_end_line;
+            // Save curve
+            curves.push([
+                center_point + vector,
+                center_point + vector + Point {
+                    x: start_end_line.x + start_end_line.y * angle_direction,
+                    y: start_end_line.y - start_end_line.x * angle_direction
+                },
+                center_point + rotated_vector + Point {
+                    x: end_start_line.x - end_start_line.y * angle_direction,
+                    y: end_start_line.x * angle_direction + end_start_line.y
+                },
+                center_point + rotated_vector
+            ]);
+            vector = rotated_vector;
+        }
+        // Generate last curve
+        if last_curve_angle != 0.0 {
+            // Calculate end point
+            let precise_vector = GenericPoint::<Degree>::from(vector);
+            let last_curve_rad = last_curve_angle.to_radians();
+            let (angle_sin, angle_cos) = (last_curve_rad.sin(), last_curve_rad.cos());
+            let precise_rotated_vector = GenericPoint {
+                x: precise_vector.x * angle_cos - precise_vector.y * angle_sin,
+                y: precise_vector.y * angle_cos + precise_vector.x * angle_sin
+            };
+            // Calculate vectors to control points
+            let start_end_line = (precise_rotated_vector - precise_vector) * CONTROL_POINT_DISTANCE;
+            let end_start_line = -start_end_line;
+            // Save curve
+            let last_curve_half_rad = last_curve_rad * 0.5;
+            let (half_angle_sin, half_angle_cos) = (last_curve_half_rad.sin(), last_curve_half_rad.cos());
+            curves.push([
+                center_point + vector,
+                center_point + vector + Point {
+                    x: (start_end_line.x * half_angle_cos + start_end_line.y * half_angle_sin) as Coordinate,
+                    y: (start_end_line.y * half_angle_cos - start_end_line.x * half_angle_sin) as Coordinate
+                },
+                center_point + Point::from(precise_rotated_vector) + Point {
+                    x: (end_start_line.x * half_angle_cos - end_start_line.y * half_angle_sin) as Coordinate,
+                    y: (end_start_line.y * half_angle_cos + end_start_line.x * half_angle_sin) as Coordinate
+                },
+                center_point + Point::from(precise_rotated_vector)
+            ]);
+        }
+        // Return collected curves
+        curves
+    } else {
+        vec![]
+    }
+}
+
 // Flatten arc to polyline
 const ARC_LINE_LENGTH: Degree = 0.75;
 pub fn flatten_arc(start_point: Point, center_point: Point, angle: Degree) -> Vec<Point> {
@@ -21,20 +97,14 @@ pub fn flatten_arc(start_point: Point, center_point: Point, angle: Degree) -> Ve
     // Add remaining points
     angle_rad /= lines_n as Degree;
     let (angle_sin, angle_cos) = (angle_rad.sin(), angle_rad.cos());
-    let mut precise_vector = GenericPoint {
-        x: vector.x as Degree,
-        y: vector.y as Degree
-    };
+    let mut precise_vector = GenericPoint::<Degree>::from(vector);
     for _ in 0..lines_n {
         points.push(center_point + {
             precise_vector = GenericPoint {
                 x: precise_vector.x * angle_cos - precise_vector.y * angle_sin,
                 y: precise_vector.x * angle_sin + precise_vector.y * angle_cos
             };
-            Point {
-                x: precise_vector.x as Coordinate,
-                y: precise_vector.y as Coordinate
-            }
+            Point::from(precise_vector)
         });
     }
     // Return points
@@ -90,7 +160,33 @@ pub fn flatten_curve(start_point: Point, control_point1: Point, control_point2: 
 // Tests
 #[cfg(test)]
 mod tests {
-    use super::{Point, flatten_curve, flatten_arc};
+    use super::{Point,arc_to_curves,flatten_curve,flatten_arc};
+
+    #[test]
+    fn arc_curves() {
+        // Short positive-angled arc
+        let curve = arc_to_curves(Point {x: 10.0, y: 10.0}, Point {x: 10.0, y: 50.0}, 45.0);
+        assert!(
+            {
+                let end_point = curve.first().expect("One curve must exist for this input!")[3];
+                (38.0..38.5).contains(&end_point.x) && (21.5..22.0).contains(&end_point.y)
+            },
+            "Short curve points unexpected: {:?}", curve
+        );
+        // Long negative-angled arc
+        let curves = arc_to_curves(Point {x: 0.0, y: 0.0}, Point {x: 0.0, y: 100.0}, -450.0);
+        assert_eq!(
+            curves.iter().map(|curve| curve[3] ).collect::<Vec<_>>(),
+            vec![
+                Point {x: -100.0, y: 100.0},
+                Point {x: 0.0, y: 200.0},
+                Point {x: 100.0, y: 100.0},
+                Point {x: 0.0, y: 0.0},
+                Point {x: -100.0, y: 100.0}
+            ],
+            "Long curve points unexpected: {:?}", curves
+        );
+    }
 
     #[test]
     fn flat_arc() {
