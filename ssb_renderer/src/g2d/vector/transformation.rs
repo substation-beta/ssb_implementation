c683b11e8e29a911ba24bcd6c8c406a3ed93a1bb
Youka <spanknebel.borken@t-online.de>
2020-01-13 21:16:12 +0100

    optimized sse for transformation

--- ssb_renderer/src/g2d/vector/transformation.rs
+++ ssb_renderer/src/g2d/vector/transformation.rs
@@ -1,32 +1,21 @@
 // Imports
-#[cfg(target_arch = "x86")]
-use std::arch::x86::*;
-#[cfg(target_arch = "x86_64")]
-use std::arch::x86_64::*;
-#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
-compile_error!("x86 or x86_64 target architecture required!");
-
 use std::ops::Mul;
 
 
-// Aligned memory for fast SSE results store
-#[repr(align(128))]
-#[derive(Default)]
-struct M128 {
-    data: [f32; 4]
-}
-
 // Matrix without any transformation
 const IDENTITY_MATRIX: [f32; 16] = [
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0,
-    0.0, 0.0, 0.0, 1.0
+    1., 0., 0., 0.,
+    0., 1., 0., 0.,
+    0., 0., 1., 0.,
+    0., 0., 0., 1.
 ];
 
 // 4x4 matrix for transformation of points
 #[derive(Debug, PartialEq, Clone)]
-#[repr(align(128))]
+#[cfg_attr(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    repr(align(128))
+)]
 pub struct Transformation {
     pub matrix: [f32; 16]
 }
@@ -40,54 +29,66 @@ impl Default for Transformation {
 impl Mul for Transformation {
     type Output = Self;
     fn mul(self, other: Self) -> Self::Output {
-        // Storages
-        let mut matrix = [0_f32; 16];
-        let mut temp = M128::default();
-        // Calculation
-        for dimension in 0..4 {
-            for column in 0..4 {
-                unsafe {
-                    _mm_store_ps(
-                        temp.data.as_mut_ptr(),
-                        _mm_mul_ps(
-                            _mm_load_ps(self.matrix.as_ptr().add(dimension << 2)),
-                            _mm_set_ps(other.matrix[12+column], other.matrix[8+column], other.matrix[4+column], other.matrix[0+column])
+        // Output buffer
+        let mut transformation = Self {
+            matrix: [0_f32; 16]
+        };
+        // Calculation with SSE
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        unsafe {
+            // Extra imports
+            #[cfg(target_arch = "x86")]
+            use std::arch::x86::*;
+            #[cfg(target_arch = "x86_64")]
+            use std::arch::x86_64::*;
+            // Preload
+            let other_row_x = _mm_load_ps(other.matrix.as_ptr());
+            let other_row_y = _mm_load_ps(other.matrix.as_ptr().add(4));
+            let other_row_z = _mm_load_ps(other.matrix.as_ptr().add(8));
+            let other_row_w = _mm_load_ps(other.matrix.as_ptr().add(12));
+            // Calculate rows
+            for row_index in (0..16).step_by(4) {
+                _mm_store_ps(
+                    transformation.matrix.as_mut_ptr().add(row_index),
+                    _mm_add_ps(
+                        _mm_mul_ps(_mm_set1_ps(self.matrix[row_index]), other_row_x),
+                        _mm_add_ps(
+                            _mm_mul_ps(_mm_set1_ps(self.matrix[row_index + 1]), other_row_y),
+                            _mm_add_ps(
+                                _mm_mul_ps(_mm_set1_ps(self.matrix[row_index + 2]), other_row_z),
+                                _mm_mul_ps(_mm_set1_ps(self.matrix[row_index + 3]), other_row_w)
+                            )
                         )
-                    );
-                    matrix[(dimension << 2) + column] = temp.data[0] + temp.data[1] + temp.data[2] + temp.data[3];
-                }
+                    )
+                );
             }
         }
-        Self {
-            matrix
+        // Calculation native
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        for row_index in (0..16).step_by(4) {
+            for column in 0..4 {
+                transformation.matrix[row_index + column] =
+                    self.matrix[row_index] * other.matrix[column] +
+                    self.matrix[row_index + 1] * other.matrix[column + 4] +
+                    self.matrix[row_index + 2] * other.matrix[column + 8] +
+                    self.matrix[row_index + 3] * other.matrix[column + 12];
+            }
         }
+        transformation
     }
 }
 impl Transformation {
     pub fn is_identity(&self) -> bool {
         self.matrix == IDENTITY_MATRIX
     }
-
     pub fn translate(self, x: f32, y: f32, z: f32) -> Self {
-        // Storages
-        let mut matrix = self.matrix.clone();
-        let mut temp = M128::default();
-        // Calculation
-        unsafe {
-            let translation = _mm_set_ps(1.0, z, y, x);
-            for dimension in 0..4 {
-                _mm_store_ps(
-                    temp.data.as_mut_ptr(),
-                    _mm_mul_ps(
-                        _mm_load_ps(self.matrix.as_ptr().add(dimension << 2)),
-                        translation
-                    )
-                );
-                matrix[(dimension << 2) + 3] = temp.data[0] + temp.data[1] + temp.data[2] + temp.data[3];
-            }
-        }
-        Self {
-            matrix
+        self * Transformation {
+            matrix: [
+                1., 0., 0., x,
+                0., 1., 0., y,
+                0., 0., 1., z,
+                0., 0., 0., 1.
+            ]
         }
     }
 
