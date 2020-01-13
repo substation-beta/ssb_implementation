4cf875a16634dfb95eff8042e4728a049134c063
Youka <spanknebel.borken@t-online.de>
2020-01-13 23:18:32 +0100

    added point (orthogonal) transform

--- ssb_renderer/src/g2d/vector/transformation.rs
+++ ssb_renderer/src/g2d/vector/transformation.rs
@@ -1,6 +1,22 @@
 // Imports
 use std::ops::Mul;
+use super::{
+    types::Coordinate,
+    point::Point
+};
+#[cfg(target_arch = "x86")]
+use std::arch::x86::*;
+#[cfg(target_arch = "x86_64")]
+use std::arch::x86_64::*;
 
+
+// Aligned memory for fast SSE result storage
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[repr(align(128))]
+#[derive(Default)]
+struct M128 {
+    data: [f32; 4]
+}
 
 // Matrix without any transformation
 const IDENTITY_MATRIX: [f32; 16] = [
@@ -36,11 +52,6 @@ impl Mul for Transformation {
         // Calculation with SSE
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         unsafe {
-            // Extra imports
-            #[cfg(target_arch = "x86")]
-            use std::arch::x86::*;
-            #[cfg(target_arch = "x86_64")]
-            use std::arch::x86_64::*;
             // Preload
             let other_row_x = _mm_load_ps(other.matrix.as_ptr());
             let other_row_y = _mm_load_ps(other.matrix.as_ptr().add(4));
@@ -142,8 +153,56 @@ impl Transformation {
             ]
         }
     }
+    pub fn transform_orthogonal(&self, point: Point, z: Coordinate) -> (Point,Coordinate) {
+        // Calculation with SSE
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            let mut result = M128::default();
+            unsafe {
+                let point4d = _mm_set_ps(1., z, point.y, point.x);
+                _mm_store_ps(
+                    result.data.as_mut_ptr(),
+                    _mm_add_ps(
+                        _mm_mul_ps(_mm_set_ps(self.matrix[12], self.matrix[8], self.matrix[4], self.matrix[0]), point4d),
+                        _mm_add_ps(
+                            _mm_mul_ps(_mm_set_ps(self.matrix[13], self.matrix[9], self.matrix[5], self.matrix[1]), point4d),
+                            _mm_add_ps(
+                                _mm_mul_ps(_mm_set_ps(self.matrix[14], self.matrix[10], self.matrix[6], self.matrix[2]), point4d),
+                                _mm_mul_ps(_mm_set_ps(self.matrix[15], self.matrix[11], self.matrix[7], self.matrix[3]), point4d)
+                            )
+                        )
+                    )
+                );
+            }
+            (
+                Point {
+                    x: result.data[0],
+                    y: result.data[1]
+                },
+                result.data[2]
+            )
+        }
+        // Calculation native
+        #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+        {
+            let result = [0_f32; 3];
+            for row_index in (0..12).step_by(4) {
+                result[row_index >> 2] =
+                    self.matrix[row_index] * point.x +
+                    self.matrix[row_index + 1] * point.y +
+                    self.matrix[row_index + 2] * z +
+                    self.matrix[row_index + 3];
+            }
+            (
+                Point {
+                    x: result[0],
+                    y: result[1]
+                },
+                result[2]
+            )
+        }
+    }
 
-    // TODO: transform_orthogonal
     // TODO: transform_perspective
 
 }
