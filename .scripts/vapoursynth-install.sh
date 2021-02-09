d9a342c51923ab9c8fadc096d6ea5cb175306cfd
Youka <spanknebel.borken@t-online.de>
2021-02-09 22:42:26 +0100

    fixed github actions for vapoursynth
    Environment variables have to be set by the new way.
    Older python version required for vapoursynth -> raised py version.

--- .scripts/vapoursynth-install.sh
+++ .scripts/vapoursynth-install.sh
@@ -17,9 +17,9 @@ download_and_build() {
 # Install system dependencies
 apt-get install -y build-essential autoconf libtool pkg-config python3-pip && pip3 install cython
 # Install zimg (from source)
-download_and_build https://github.com/sekrit-twc/zimg/archive/release-2.9.2.tar.gz zimg-release-2.9.2
+download_and_build https://github.com/sekrit-twc/zimg/archive/release-2.9.3.tar.gz zimg-release-2.9.3
 # Install vapoursynth (from source)
-download_and_build https://github.com/vapoursynth/vapoursynth/archive/R47.2.tar.gz vapoursynth-R47.2
+download_and_build https://github.com/vapoursynth/vapoursynth/archive/R52.tar.gz vapoursynth-R52
 # Fix vapoursynth (native) python path
 PYTHON3_LOCAL_LIB_PATH=$(echo /usr/local/lib/python3.*)
 ln -s $PYTHON3_LOCAL_LIB_PATH/site-packages/vapoursynth.so $PYTHON3_LOCAL_LIB_PATH/dist-packages/vapoursynth.so
