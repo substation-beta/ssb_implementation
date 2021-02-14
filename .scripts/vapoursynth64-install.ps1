438903a1092a6927ee014965183eafec721fb0df
Quan Pham <doraemonfanclub@gmail.com>
2021-02-14 21:47:35 +0700

    Restore Out-File command

--- .scripts/vapoursynth64-install.ps1
+++ .scripts/vapoursynth64-install.ps1
@@ -7,6 +7,6 @@ Invoke-WebRequest https://github.com/vapoursynth/vapoursynth/releases/download/R
 # Show vapoursynth version
 python -c "import vapoursynth; print(vapoursynth.core.version())"
 # Add compiler access to vapoursynth sdk
-echo "VAPOURSYNTH_LIB_DIR=$Env:PYTHON/sdk/lib64"
+echo "VAPOURSYNTH_LIB_DIR=$Env:PYTHON/sdk/lib64" | Out-File -FilePath $Env:GITHUB_ENV -Append -Encoding utf8    # https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable
 # Cleanup
 Remove-Item VapourSynth64-Portable-R52.7z
