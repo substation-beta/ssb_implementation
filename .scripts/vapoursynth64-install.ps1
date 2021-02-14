f78d4df99c1763cbcead0178e929d27d99663abc
Quan Pham <doraemonfanclub@gmail.com>
2021-02-14 21:42:51 +0700

    Re-add filename

--- .scripts/vapoursynth64-install.ps1
+++ .scripts/vapoursynth64-install.ps1
@@ -1,7 +1,7 @@
 # Get python home
 $Env:PYTHON = split-path (Get-Command python).Source
 # Load vapoursynth R52
-Invoke-WebRequest https://github.com/vapoursynth/vapoursynth/releases/download/R52/VapourSynth64-Portable-R52.7z
+Invoke-WebRequest https://github.com/vapoursynth/vapoursynth/releases/download/R52/VapourSynth64-Portable-R52.7z -OutFile "VapourSynth64-Portable-R52.7z"
 # Extract vapoursynth archive into python
 7z x VapourSynth64-Portable-R52.7z -o"$Env:PYTHON" -y '-xr!7z.*'
 # Show vapoursynth version
@@ -9,4 +9,4 @@ python -c "import vapoursynth; print(vapoursynth.core.version())"
 # Add compiler access to vapoursynth sdk
 echo "VAPOURSYNTH_LIB_DIR=$Env:PYTHON/sdk/lib64"
 # Cleanup
-# Remove-Item VapourSynth64-Portable-R52.7z
+Remove-Item VapourSynth64-Portable-R52.7z
