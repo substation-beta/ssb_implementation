2aa0b941554c989442095a33ad8ed8cbced18872
Quan Pham <doraemonfanclub@gmail.com>
2021-02-14 21:18:43 +0700

    Minor enhancement for PowerShell script and gitignore file

--- .scripts/vapoursynth64-install.ps1
+++ .scripts/vapoursynth64-install.ps1
@@ -1,10 +1,12 @@
 # Get python home
 $Env:PYTHON = split-path (Get-Command python).Source
 # Load vapoursynth R52
-wget https://github.com/vapoursynth/vapoursynth/releases/download/R52/VapourSynth64-Portable-R52.7z -Outfile VapourSynth64-Portable-R52.7z
+curl -o VapourSynth64-Portable-R52.7z -LJO https://github.com/vapoursynth/vapoursynth/releases/download/R52/VapourSynth64-Portable-R52.7z
 # Extract vapoursynth archive into python
-7z x VapourSynth64-Portable-R52.7z -o"$Env:PYTHON" -y
+7z x VapourSynth64-Portable-R52.7z -o"$Env:PYTHON" -y '-xr!7z.*'
 # Show vapoursynth version
 python -c "import vapoursynth; print(vapoursynth.core.version())"
 # Add compiler access to vapoursynth sdk
-echo "VAPOURSYNTH_LIB_DIR=$Env:PYTHON/sdk/lib64" | Out-File -FilePath $Env:GITHUB_ENV -Append -Encoding utf8    # https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable
+echo "VAPOURSYNTH_LIB_DIR=$Env:PYTHON/sdk/lib64"
+# Cleanup
+Remove-Item VapourSynth64-Portable-R52.7z
