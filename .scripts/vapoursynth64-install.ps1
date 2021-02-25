49b34c026c60a78af416fbc8d66bbf67bba82a71
Quan Pham <doraemonfanclub@gmail.com>
2021-02-25 10:17:47 +0700

    Revert to R47

--- .scripts/vapoursynth64-install.ps1
+++ .scripts/vapoursynth64-install.ps1
@@ -1,12 +1,10 @@
 # Get python home
 $Env:PYTHON = split-path (Get-Command python).Source
-# Load vapoursynth R52
-Invoke-WebRequest https://github.com/vapoursynth/vapoursynth/releases/download/R52/VapourSynth64-Portable-R52.7z -OutFile "VapourSynth64-Portable-R52.7z"
+# Load vapoursynth R47.2
+wget https://github.com/vapoursynth/vapoursynth/releases/download/R47.2/VapourSynth64-Portable-R47.2.7z -Outfile VapourSynth64-Portable-R47.2.7z
 # Extract vapoursynth archive into python
-7z x VapourSynth64-Portable-R52.7z -o"$Env:PYTHON" -y -aos '-xr!7z.*'
+7z x VapourSynth64-Portable-R47.2.7z -o"$Env:PYTHON" -y
 # Show vapoursynth version
 python -c "import vapoursynth; print(vapoursynth.core.version())"
 # Add compiler access to vapoursynth sdk
 echo "VAPOURSYNTH_LIB_DIR=$Env:PYTHON/sdk/lib64" | Out-File -FilePath $Env:GITHUB_ENV -Append -Encoding utf8    # https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable
-# Cleanup
-Remove-Item VapourSynth64-Portable-R52.7z
