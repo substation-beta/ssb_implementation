415caaaac3dba16b98814dd488242f824dc36f9f
Quan Pham <doraemonfanclub@gmail.com>
2021-02-25 09:56:07 +0700

    Exp: Use latest vapoursynth source snapshot

--- .scripts/vapoursynth-install.sh
+++ .scripts/vapoursynth-install.sh
@@ -19,7 +19,7 @@ apt-get install -y build-essential autoconf libtool pkg-config python3-pip && pi
 # Install zimg (from source)
 download_and_build https://github.com/sekrit-twc/zimg/archive/release-2.9.3.tar.gz zimg-release-2.9.3
 # Install vapoursynth (from source)
-download_and_build https://github.com/vapoursynth/vapoursynth/archive/R47.2.tar.gz vapoursynth-R47.2
+download_and_build https://github.com/vapoursynth/vapoursynth/archive/2f0a78495608424019b7a85510699ef68d8484c2.tar.gz vapoursynth-R52
 # Fix vapoursynth (native) python path
 PYTHON3_LOCAL_LIB_PATH=$(echo /usr/local/lib/python3.*)
 ln -s $PYTHON3_LOCAL_LIB_PATH/site-packages/vapoursynth.so $PYTHON3_LOCAL_LIB_PATH/dist-packages/vapoursynth.so
